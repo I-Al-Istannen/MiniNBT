7af4cd1e13cabfceff74ff6312a446cc914d4643
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2019-09-22 23:16:14 +0200

    Migrate the NBT parser to the fluent reflection utility

---
+++ src/test/java/me/ialistannen/mininbt/NbtParserTest.java
@@ -0,0 +1,33 @@
+package me.ialistannen.mininbt;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import me.ialistannen.mininbt.NBTWrappers.NBTTagCompound;
+import me.ialistannen.mininbt.NBTWrappers.NBTTagList;
+import me.ialistannen.mininbt.NBTWrappers.NBTTagString;
+import me.ialistannen.mininbt.NbtParser.NbtParseException;
+import org.junit.jupiter.api.Test;
+
+class NbtParserTest {
+
+  @Test
+  public void roundtripSimpleCompound() throws NbtParseException {
+    NBTTagCompound compound = new NBTTagCompound();
+    compound.setString("hello", "World");
+    compound.setBoolean("you", true);
+    compound.setByte("byte", (byte) 20);
+    compound.setDouble("double", 1.245);
+    compound.setIntArray("ints", new int[]{1, 5, 1, -20});
+    NBTTagList list = new NBTTagList();
+    list.add(new NBTTagString("MyList"));
+    list.add(new NBTTagString("There"));
+    compound.set("list", list);
+
+    String asString = compound.toNBT().toString();
+
+    assertEquals(
+        compound,
+        NbtParser.parse(asString)
+    );
+  }
+}
